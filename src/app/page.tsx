be92978c0f5d950ee012c20df09b88b0323b6e19
tcooc <tcooc@users.noreply.github.com>
2024-01-14 09:20:53 -0500

    Disable SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,6 @@
 "use client";
-import { CssBaseline, useMediaQuery } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useMemo } from "react";
-import { EngravingApp } from "@/engravings";
+import dynamic from "next/dynamic";
 
-export default function App() {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: light)");
-
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? "light" : "dark",
-        },
-        components: {
-          MuiFormControl: {
-            defaultProps: { variant: "outlined", size: "small" },
-          },
-          MuiDialog: {
-            styleOverrides: {
-              paper: {
-                maxWidth: 800,
-              },
-            },
-          },
-        },
-      }),
-    [prefersDarkMode]
-  );
-
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <EngravingApp />
-    </ThemeProvider>
-  );
-}
+export default dynamic(() => import("@/App"), {
+  ssr: false,
+});
